cf5c8bf429d9ec7935009494e0e1b6ce6357409b
minorObsession <terzicbogdan95@gmail.com>
2024-12-10 09:15:50 -0800

    To merge w Dev

--- src/js/views/ModalView.js
+++ src/js/views/ModalView.js
@@ -119,7 +119,7 @@ export default class ModalView {
       const isValid = await this._validator.isValid;
       console.log('is the form valid', isValid);
       if (isValid) {
-        const formData = this.getFormData();
+        const formData = this._getFormData();
         // console.log(formData);
 
         if (onSuccess) onSuccess(formData);
